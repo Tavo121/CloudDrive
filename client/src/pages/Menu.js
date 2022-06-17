69ab8b66099444cf3e70aa35010c872355dcb5d3
Sebas36762 <sebas4101p@estudiantec.cr>
2022-06-17 15:46:45 -0600

    Login
    Creacion del login con mecanismos de autenticación

---
+++ client/src/pages/Menu.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
+
+class Menu extends Component {
+    cerrarSesion=()=>{
+        cookies.remove('id', {path: "/"});
+        cookies.remove('apellido1', {path: "/"});
+        cookies.remove('apellido2', {path: "/"});
+        cookies.remove('nombre', {path: "/"});
+        cookies.remove('username', {path: "/"});
+        window.location.href='./';
+    }
+
+    componentDidMount() {
+        if(!cookies.get('username')){
+            window.location.href="./";
+        }
+    }
+
+    render() {
+        console.log('id: '+ cookies.get('id'));
+        console.log('apellido1: '+cookies.get('apellido1'));
+        console.log('apellido2: '+cookies.get('apellido2'));
+        console.log('nombre: '+cookies.get('nombre'));
+        console.log('username: '+cookies.get('username'));
+        return (
+            <div>
+                Menu Principal
+
+                <br />
+                <button onClick={()=>this.cerrarSesion()}>Cerrar Sesión</button>
+            </div>
+        );
+    }
+}
+
+export default Menu;
