2353070442cafed8371fdf4e3ac230717320c290
Sebas36762 <sebas4101p@estudiantec.cr>
2022-06-17 22:35:54 -0600

    Menu

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Cookies from 'universal-cookie';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/Menu.css';
 
 const cookies = new Cookies();
 
@@ -25,12 +28,54 @@ class Menu extends Component {
         console.log('apellido2: '+cookies.get('apellido2'));
         console.log('nombre: '+cookies.get('nombre'));
         console.log('username: '+cookies.get('username'));
+
+
+        /*
+        const [archivos,setArchivos]=useState(null); 
+
+        const subirArchivos=e=>{
+            setArchivos(e);
+        }
+
+        const insertarArchivos=async()=>{
+            const f = new FormData();
+
+            for (let index = 0; index < archivos.length; index++) {
+                f.append("files",archivos[index]);
+                
+            }
+            await axios.post("http://localhost:3000/Menu",f, {headers: {'Content-Type':'multipart/form-data'}})
+            .then(response=>{
+                console.log(response.data);
+            }).catch(error=>{
+                console.log(error);
+            })
+        }*/
+        /**onChange={(e)=>subirArchivos(e.target.files)}
+
+
+        onClick={()=>insertarArchivos()}
+        */
         return (
             <div>
                 Menu Principal
+                <div className='Subida_Archivos'>                
+                    <br></br>
+                    <input type='file' name='files' multiple/>
+                    <br></br>
+                    <br></br>
+                    <button className='btn btn-primary'>Subir Archivo</button>
 
-                <br />
-                <button onClick={()=>this.cerrarSesion()}>Cerrar Sesión</button>
+                    <button style={{marginLeft: 100}}>Descargar</button>
+                </div>
+
+
+                <br>
+                </br>
+
+                <br></br>
+                <br></br>
+                <button  onClick={()=>this.cerrarSesion()}>Cerrar Sesión</button>
             </div>
         );
     }
